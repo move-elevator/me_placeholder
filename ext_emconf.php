3961d6b9c78961fe6433c355d9f30f13cd4d66ee
Jan Männig <janmaennig@gmail.com>
2015-02-03 10:06:45 +0100

    Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "me_placeholder".
+#
+# Auto generated 09-10-2012 13:49
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'move:elevator Platzhalter im Frontendcontent',
+	'description' => 'Ersätzt Platzhalte im Conten durch andere Inhalte.',
+	'category' => 'be',
+	'author' => 'Jan Maennig',
+	'author_email' => 'jma@move-elevator.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"d516";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"621c";s:14:"ext_tables.php";s:4:"652c";s:14:"ext_tables.sql";s:4:"83e3";s:33:"icon_tx_meplaceholder_records.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"fc9a";s:10:"README.txt";s:4:"ee2d";s:7:"tca.php";s:4:"e998";s:19:"doc/wizard_form.dat";s:4:"4184";s:20:"doc/wizard_form.html";s:4:"bf1c";}',
+);
+
+?>
