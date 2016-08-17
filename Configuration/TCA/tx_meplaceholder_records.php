258b8534852a7d9cbf314fb2579c2fd39cf0e61c
Sascha Seyfert <sef@move-elevator.de>
2016-08-17 16:41:05 +0200

    MEWSRL-351 change cgl to PSR2

--- Configuration/TCA/tx_meplaceholder_records.php
+++ Configuration/TCA/tx_meplaceholder_records.php
@@ -1,92 +1,92 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 $TCA['tx_meplaceholder_records'] = array(
-	'ctrl' => $TCA['tx_meplaceholder_records']['ctrl'],
-	'interface' => array(
-		'showRecordFieldList' => 'hidden,starttime,endtime,fe_group,placeholder,content'
-	),
-	'feInterface' => $TCA['tx_meplaceholder_records']['feInterface'],
-	'columns' => array(
-		'hidden' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
-			'config' => array(
-				'type' => 'check',
-				'default' => '0'
-			)
-		),
-		'starttime' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.starttime',
-			'config' => array(
-				'type' => 'input',
-				'size' => '8',
-				'max' => '20',
-				'eval' => 'date',
-				'default' => '0',
-				'checkbox' => '0'
-			)
-		),
-		'endtime' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.endtime',
-			'config' => array(
-				'type' => 'input',
-				'size' => '8',
-				'max' => '20',
-				'eval' => 'date',
-				'checkbox' => '0',
-				'default' => '0',
-				'range' => array(
-					'upper' => mktime(3, 14, 7, 1, 19, 2038),
-					'lower' => mktime(0, 0, 0, date('m') - 1, date('d'), date('Y'))
-				)
-			)
-		),
-		'fe_group' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.fe_group',
-			'config' => array(
-				'type' => 'select',
-				'items' => array(
-					array('', 0),
-					array('LLL:EXT:lang/locallang_general.xml:LGL.hide_at_login', -1),
-					array('LLL:EXT:lang/locallang_general.xml:LGL.any_login', -2),
-					array('LLL:EXT:lang/locallang_general.xml:LGL.usergroups', '--div--')
-				),
-				'foreign_table' => 'fe_groups'
-			)
-		),
-		'placeholder' => array(
-			'exclude' => 0,
-			'label' => 'LLL:EXT:me_placeholder/Resources/Private/Language/locallang_db.xlf:tx_meplaceholder_records.placeholder',
-			'config' => array(
-				'type' => 'input',
-				'size' => '30',
-				'max' => '20',
-				'eval' => 'required,trim,alphanum_x,upper',
-			)
-		),
-		'content' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:me_placeholder/Resources/Private/Language/locallang_db.xlf:tx_meplaceholder_records.content',
-			'defaultExtras' => 'richtext',
-			'config' => array(
-				'type' => 'text',
-				'eval' => 'required',
-				'rows' => '5',
-				'cols' => 48,
-			),
-			'defaultExtras' => 'richtext[*]'
-		),
-	),
-	'types' => array(
-		'0' => array('showitem' => 'hidden;;1;;1-1-1, placeholder, content')
-	),
-	'palettes' => array(
-		'1' => array('showitem' => 'starttime, endtime, fe_group')
-	)
+    'ctrl' => $TCA['tx_meplaceholder_records']['ctrl'],
+    'interface' => array(
+        'showRecordFieldList' => 'hidden,starttime,endtime,fe_group,placeholder,content'
+    ),
+    'feInterface' => $TCA['tx_meplaceholder_records']['feInterface'],
+    'columns' => array(
+        'hidden' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
+            'config' => array(
+                'type' => 'check',
+                'default' => '0'
+            )
+        ),
+        'starttime' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.starttime',
+            'config' => array(
+                'type' => 'input',
+                'size' => '8',
+                'max' => '20',
+                'eval' => 'date',
+                'default' => '0',
+                'checkbox' => '0'
+            )
+        ),
+        'endtime' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.endtime',
+            'config' => array(
+                'type' => 'input',
+                'size' => '8',
+                'max' => '20',
+                'eval' => 'date',
+                'checkbox' => '0',
+                'default' => '0',
+                'range' => array(
+                    'upper' => mktime(3, 14, 7, 1, 19, 2038),
+                    'lower' => mktime(0, 0, 0, date('m') - 1, date('d'), date('Y'))
+                )
+            )
+        ),
+        'fe_group' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.fe_group',
+            'config' => array(
+                'type' => 'select',
+                'items' => array(
+                    array('', 0),
+                    array('LLL:EXT:lang/locallang_general.xml:LGL.hide_at_login', -1),
+                    array('LLL:EXT:lang/locallang_general.xml:LGL.any_login', -2),
+                    array('LLL:EXT:lang/locallang_general.xml:LGL.usergroups', '--div--')
+                ),
+                'foreign_table' => 'fe_groups'
+            )
+        ),
+        'placeholder' => array(
+            'exclude' => 0,
+            'label' => 'LLL:EXT:me_placeholder/Resources/Private/Language/locallang_db.xlf:tx_meplaceholder_records.placeholder',
+            'config' => array(
+                'type' => 'input',
+                'size' => '30',
+                'max' => '20',
+                'eval' => 'required,trim,alphanum_x,upper',
+            )
+        ),
+        'content' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:me_placeholder/Resources/Private/Language/locallang_db.xlf:tx_meplaceholder_records.content',
+            'defaultExtras' => 'richtext',
+            'config' => array(
+                'type' => 'text',
+                'eval' => 'required',
+                'rows' => '5',
+                'cols' => 48,
+            ),
+            'defaultExtras' => 'richtext[*]'
+        ),
+    ),
+    'types' => array(
+        '0' => array('showitem' => 'hidden;;1;;1-1-1, placeholder, content')
+    ),
+    'palettes' => array(
+        '1' => array('showitem' => 'starttime, endtime, fe_group')
+    )
 );
--- ext_tables.php
+++ ext_tables.php
@@ -1,28 +1,28 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_meplaceholder_records');
 
 $TCA['tx_meplaceholder_records'] = array(
-	'ctrl' => array(
-		'title' => 'LLL:EXT:me_placeholder/Resources/Private/Language/locallang_db.xlf:tx_meplaceholder_records',
-		'label' => 'placeholder',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY crdate',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-			'fe_group' => 'fe_group',
-		),
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) .
-			'Configuration/TCA/tx_meplaceholder_records.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) .
-			'Resources/Public/Icons/tx_meplaceholder_records.gif',
-	),
+    'ctrl' => array(
+        'title' => 'LLL:EXT:me_placeholder/Resources/Private/Language/locallang_db.xlf:tx_meplaceholder_records',
+        'label' => 'placeholder',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'default_sortby' => 'ORDER BY crdate',
+        'delete' => 'deleted',
+        'enablecolumns' => array(
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+            'fe_group' => 'fe_group',
+        ),
+        'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) .
+            'Configuration/TCA/tx_meplaceholder_records.php',
+        'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) .
+            'Resources/Public/Icons/tx_meplaceholder_records.gif',
+    ),
 );
